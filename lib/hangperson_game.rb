9912ba24cacf5f41eaf280f6f07b7adf2d1042e3
Gonçalo Sampaio <gsampaio@wesleyan.edu>
2016-02-13 23:38:35 +0000

    Hangperson_game finished

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -8,10 +8,62 @@ class HangpersonGame
   # def initialize()
   # end
   
+  attr_accessor :word, :guesses, :wrong_guesses
+  
   def initialize(word)
     @word = word
+    @guesses = ''
+    @wrong_guesses = ''
   end
 
+  def is_valid?(letter)
+  	return letter =~ /^[a-z]$/i
+  end
+  
+  def guess(letter)
+    unless is_valid? letter
+  		raise ArgumentError
+    end
+    
+    letter.downcase!
+    
+    is_a_different_letter = false
+    
+    if self.word.include? letter
+      unless self.guesses.include? letter
+        self.guesses += letter
+        is_a_different_letter = true
+      end
+    else
+      unless self.wrong_guesses.include? letter
+        self.wrong_guesses += letter
+        is_a_different_letter = true
+      end
+    end
+    return is_a_different_letter
+  end
+
+  def word_with_guesses
+    word_guessed = self.word.dup
+    self.word.split('').uniq.each do |letter|
+      unless self.guesses.include? letter
+  		  word_guessed.gsub!(letter, '-')
+  	  end
+  	end
+  	return word_guessed
+  end
+  
+  def check_win_or_lose
+    status = :play
+  	if self.word == word_with_guesses
+  	  status = :win
+  	end
+  	if self.wrong_guesses.length >= 7
+  	  status = :lose
+  	end
+  	return status
+  end
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
